33cfda3b32f1aa272b7999a0bd6532e47814d8e5
Kaushal Parekh <kaushal.parekh@dkfz-heidelberg.de>
2021-10-04 09:16:13 +0200

    Fix bug in CI failure email that doesn't show CI launches link

--- CI/scripts/error_handler.py
+++ CI/scripts/error_handler.py
@@ -126,7 +126,7 @@ def ci_failure_notification(message="", logs_dict={}):
                 This is to inform you that the Kaapana CI has failed. <b> {} </b>.<br>
                 You are receiving this email since you are a part of the Kaapana team.<br>
                 To view the CI launches, visit the following link:<br>
-                <http://10.128.130.252/ui/#kaapana/launches/all><br>
+                http://10.128.130.252/ui/#kaapana/launches/all<br>
                 and use the following credentials to login:<br>
                 user: read<br>
                 password: Read_1234<br>
